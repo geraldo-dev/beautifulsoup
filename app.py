7704dfaf4ba1adac9dc14741ea91bec7bf4d32e8
geraldo sousa <geraldosousadev@gmail.com>
2022-09-09 23:35:24 -0300

    browsing tags by beautifulsoup

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.edge.options import Options
 from bs4 import BeautifulSoup as bs
 from time import sleep
 
-drive = webdriver.ChromiumEdge()
+options = Options()
+options.add_argument('--headless')
+
+drive = webdriver.ChromiumEdge(options=options)
 
 drive.get('https://github.com/geraldo-dev')
 page = bs(drive.page_source, 'html.parser')
 
 sleep(2)
 
-print(page.prettify())
+title_page = page.title.text
+d = page.find("h1").find_all('span')[-1].text
+
+print(type(d),d)
