a8ebd3ae9c2f4014c7730c905c3143565bcefc92
geraldo sousa <geraldosousadev@gmail.com>
2022-09-10 22:43:24 -0300

    organizing code with classes

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2 import driver
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options
 from bs4 import BeautifulSoup as bs
@@ -8,13 +9,27 @@ options.add_argument('--headless')
 
 drive = webdriver.ChromiumEdge(options=options)
 
-drive.get('https://github.com/geraldo-dev')
-sleep(2)
-page = bs(drive.page_source, 'html.parser')
+class UserGitHub:
+    def __init__(self, drive):
+        self.drive = drive
 
+    def find_user_github(self, nick):
+        dados = {}
+        self.drive.get(f'https://github.com/{nick}')
+        sleep(2)
+        page = bs(self.drive.page_source, 'html.parser')
+        
 
-name = page.find("span", class_='p-name vcard-fullname d-block overflow-hidden')
-nick_name = page.find("span", class_="p-nickname vcard-username d-block")
-description = page.find("div", class_="p-note user-profile-bio mb-3 js-user-profile-bio f4")
+        name = page.find("span", class_='p-name vcard-fullname d-block overflow-hidden')
+        nick_name = page.find("span", class_="p-nickname vcard-username d-block")
+        description = page.find("div", class_="p-note user-profile-bio mb-3 js-user-profile-bio f4")
 
-print('-->', nick_name.get_text())
+        dados['name'] = name.text
+        dados['nick_name'] = nick_name.text
+        dados['description'] = description.text
+
+        return dados
+        
+
+user = UserGitHub(drive)
+print(user.find_user_github('geraldo-dev'))
